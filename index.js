6d2faa08a00fc32ab072f6146c52597ef4286f08
JLEONARDCS <leonardo_cruz15@hotmail.com>
2023-10-07 21:04:31 -0300

    feat: add soma function

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("hello world modify")
 console.log("add modificação em desenvolvimento")
+function soma(a, b) {
+    return a + b
+}
+const result = soma(1, 2)
+console.log("[result]", result)
